b7a77f488a1673e3451b9f1821981d59a649bd0d
Kyle Burrows <dotnetKyle@gmail.com>
2022-09-18 14:32:32 -0400

    Added Dir, Group, Table, Time, and Trace

---
+++ src/BlazorDemoApp/TestObject.cs
@@ -0,0 +1,6 @@
+﻿public class TestObject
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Test1 { get; set; } = "";
+    public string Test2 { get; set; } = "";
+}
--- src/WASM.Console/BrowserConsole.cs
+++ src/WASM.Console/BrowserConsole.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.JSInterop;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace WASM.Console;
 
@@ -13,47 +15,62 @@ public class BrowserConsole
     }
 
     public async void Log(string message)
-    {
-        await js.InvokeVoidAsync("console.log", message);
-    }
+        => await js.InvokeVoidAsync("console.log", message);
+    
     public async void Debug(string message)
-    {
-        await js.InvokeVoidAsync("console.debug", message);
-    }
+        => await js.InvokeVoidAsync("console.debug", message);
 
     public async void Error(string message)
-    {
-        await js.InvokeVoidAsync("console.error", message);
-    }
+        =>  await js.InvokeVoidAsync("console.error", message);
 
     public async void Info(string message)
-    {
-        await js.InvokeVoidAsync("console.info", message);
-    }
+        => await js.InvokeVoidAsync("console.info", message);
 
     public async void Warn(string message)
-    {
-        await js.InvokeVoidAsync("console.warn", message);
-    }
+        => await js.InvokeVoidAsync("console.warn", message);
 
     public async void Clear()
-    {
-        await js.InvokeVoidAsync("console.clear");
-    }
+        => await js.InvokeVoidAsync("console.clear");
+
     public async void Count()
-    {
-        await js.InvokeVoidAsync("console.count");
-    }
+        => await js.InvokeVoidAsync("console.count");
     public async void Count(string label)
-    {
-        await js.InvokeVoidAsync("console.count", label);
-    }
+        => await js.InvokeVoidAsync("console.count", label);
+
     public async void CountReset()
-    {
-        await js.InvokeVoidAsync("console.countReset");
-    }
+        => await js.InvokeVoidAsync("console.countReset");
     public async void CountReset(string label)
-    {
-        await js.InvokeVoidAsync("console.countReset", label);
-    }
+        => await js.InvokeVoidAsync("console.countReset", label);
+
+    public async void Dir(object obj)
+        => await js.InvokeVoidAsync("console.dir", obj);
+
+    public async void Group()
+        => await js.InvokeVoidAsync("console.group");
+    public async void Group(string label)
+        => await js.InvokeVoidAsync("console.group", label);
+
+    public async void GroupEnd()
+        => await js.InvokeVoidAsync("console.groupEnd");
+    public async void GroupEnd(string label)
+        => await js.InvokeVoidAsync("console.groupEnd", label);
+
+    public async void GroupCollapsed()
+            => await js.InvokeVoidAsync("console.groupCollapsed");
+    public async void GroupCollapsed(string label)
+        => await js.InvokeVoidAsync("console.groupCollapsed", label);
+
+    public async void Table(IEnumerable<object> objs)
+        => await js.InvokeVoidAsync("console.table", objs);
+    public async void Table<TObject>(IEnumerable<TObject> objs)
+        => await js.InvokeVoidAsync("console.table", objs);
+
+    public async void Time(string label)
+        => await js.InvokeVoidAsync("console.time", label);
+    public async void TimeEnd(string label)
+        => await js.InvokeVoidAsync("console.timeEnd", label);
+
+    public async void Trace()
+        => await js.InvokeVoidAsync("console.trace");
+
 }
